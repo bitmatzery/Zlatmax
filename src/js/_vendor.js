7e77a64747d42b142c93fc85fbcfb5cb7bf86a8e
bitmatzery <bitmatzery@gmail.com>
2023-05-09 19:26:44 +0300

    add rating

--- src/js/_vendor.js
+++ src/js/_vendor.js
@@ -1,4 +1,5 @@
 import '../../node_modules/swiper/swiper-bundle.min.js';
 import './vendor/spoller';
+import './vendor/rating';
 import { useDynamicAdapt } from './vendor/dynamicAdapt.js';
 useDynamicAdapt();
