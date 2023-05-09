9c57ca26e0d901dcaec14a2eebf16c93d9bf5552
bitmatzery <bitmatzery@gmail.com>
2023-05-09 19:27:53 +0300

    add catalog-crds, products-slider

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,30 +1,24 @@
-// Слайдер Swiper
 import Swiper, { Navigation, Pagination, Parallax, Autoplay, Thumbs } from 'swiper';
 
 // main-block__slider
 if (document.querySelector('.main-block__slider')) {
   const swiper = new Swiper('.main-block__slider', {
     modules: [Pagination, Parallax, Autoplay],
-    slidesPerView: 1, //'auto',
+    slidesPerView: 1,
     spaceBetween: 50,
-    // direction: 'vertical',
     loop: true,
     observer: true,
     observeParents: true,
     parallax: true,
-    // speed: 800,
-    // dots: true,
-    // arrows: false,
     fade: true,
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
     },
-    // autoHeight: true,
+
     slideClass: 'main-block__slide',
     wrapperClass: 'main-block__swiper',
 
-    // If we need pagination
     pagination: {
       el: '.controll-main-block__dotts',
       clickable: true,
@@ -33,19 +27,12 @@ if (document.querySelector('.main-block__slider')) {
     on: {
       init: function (swiper) {
         const allSlides = document.querySelector('.fraction-controll__all');
-        // с вычетом дубликатов слайдов при вкл loop способ получения кол-ва слайдов для фракции
         const allSlidesItems = document.querySelectorAll('.slide-main-block:not(.swiper-slide-duplicate)');
         allSlides.innerHTML = allSlidesItems.length < 10 ? `0${allSlidesItems.length}` : swiper.slides.length;
-        // обычный способ получения кол-ва слайдов для фракции
-        //const allSlides = document.querySelector('.fraction-controll__all');
-        //allSlides.innerHTML = swiper.slides.length < 10 ? `0${swiper.slides.length}` : swiper.slides.length;
       },
       slideChange: function (swiper) {
         const currentSlides = document.querySelector('.fraction-controll__current');
-        // с вычетом дубликатов слайдов при вкл loop способ получения кол-ва слайдов для фракции
         currentSlides.innerHTML = swiper.realIndex + 1 < 10 ? `0${swiper.realIndex + 1}` : swiper.realIndex + 1;
-        // обычный способ получения кол-ва слайдов для фракции
-        //currentSlides.innerHTML = swiper.activeIndex + 1 < 10 ? `0${swiper.activeIndex + 1}` : swiper.activeIndex + 1;
         console.log(swiper);
       },
     },
@@ -53,6 +40,50 @@ if (document.querySelector('.main-block__slider')) {
 
   swiper.on('swiper-slide', function () {
     console.log('Слайд переключён');
+  });
+}
+
+// products-slider
+if (document.querySelector('.products-slider__slider')) {
+  new Swiper('.products-slider__slider', {
+    modules: [Navigation, Pagination, Parallax, Autoplay],
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    watchOverflow: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    observer: true,
+    observeParents: true,
+
+    slideClass: 'products-slider__slide',
+    wrapperClass: 'products-slider__swiper',
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1370: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    },
+
+    pagination: {
+      el: '.products-slider__dotts',
+      clickable: true,
+      dynamicBullets: true,
+    },
   });
 }
 
