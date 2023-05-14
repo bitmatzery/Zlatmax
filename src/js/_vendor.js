8d59bdc612a3caf4bd7f85b35e2195ebf166b9ad
bitmatzery <bitmatzery@gmail.com>
2023-05-14 18:12:49 +0300

    add range

--- src/js/_vendor.js
+++ src/js/_vendor.js
@@ -1,5 +1,6 @@
 import '../../node_modules/swiper/swiper-bundle.min.js';
 import './vendor/spoller';
 import './vendor/rating';
+import './vendor/range';
 import { useDynamicAdapt } from './vendor/dynamicAdapt.js';
 useDynamicAdapt();
