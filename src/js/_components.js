ad09d80bdf57152225bafe262801cf8e609890d4
bitmatzery <bitmatzery@gmail.com>
2023-05-25 09:56:39 +0300

    add quantity

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,4 +2,6 @@ import './components/header-menu';
 import './components/burger-menu';
 import './components/tippy';
 import './components/slider';
-
+import './components/quantity';
+import GraphTabs from 'graph-tabs';
+const tabs = new GraphTabs('tab');
